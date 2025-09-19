fdd992cc0f29343a7d28944962a5a6a7292d41f4
HT <chenyuw2019@gmail.com>
2025-09-19 14:19:11 -0400

    Added a method to add the unpaidHours to test whether paySalary works properly

--- Employee.java
+++ Employee.java
@@ -55,6 +55,10 @@ public class Employee{
         }
     }
     
+    public void addUnpaidHours(double hours){
+        unpaidHours += hours;
+    }
+    
     /*
      * Returns the net pay for the outstanding unpaid hours
      */
